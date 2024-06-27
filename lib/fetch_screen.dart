a29bc8d4177b00fc0441c54066f32b1f3643a481
sumeetbharali <sumeetbharali2000@gmail.com>
2024-06-27 23:22:32 +0530

    final

---
+++ lib/fetch_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:assamesejwellery/screens/btm_bar.dart';
+import 'package:provider/provider.dart';
+
+import 'providers/products_provider.dart';
+
+class FetchScreen extends StatefulWidget {
+  const FetchScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FetchScreen> createState() => _FetchScreenState();
+}
+
+class _FetchScreenState extends State<FetchScreen> {
+  @override
+  void initState() {
+    Future.delayed( Duration(microseconds: 5), () async {
+      final productsProvider =
+          Provider.of<ProductsProvider>(context, listen: false);
+      await productsProvider.fetchProducts();
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (ctx) => const BottomBarScreen(),
+      ));
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset(
+            'assets/images/landing/buyfood.jpg',
+            fit: BoxFit.cover,
+            height: double.infinity,
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.7),
+          ),
+          const Center(
+            child: SpinKitFadingFour(
+              color: Colors.white,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
